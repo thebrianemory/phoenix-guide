b35c6a2f9d23ee343d65cf10a83e17a28a04762b
thebrianemory <the@brianemory.com>
2017-04-22 11:39:28 -0700

    Up to rendering an admin layout

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,8 +3,14 @@ defmodule HelloPhoenix.PageController do
 
   def index(conn, _params) do
     conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend we have an error")
-    |> render("index.html")
+    |> put_layout("admin.html")
+    |> render "index.html"
+
+    # |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    # |> put_flash(:error, "Let's pretend we have an error")
+    # |> render("index.html")
+
+    # |> put_layout(false)
+    # |> render "index.html"
   end
 end
