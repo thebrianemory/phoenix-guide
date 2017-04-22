755de22584cfc40249d36a2e21306f4ecabb7ae2
thebrianemory <the@brianemory.com>
2017-04-22 11:53:52 -0700

    Overriding Rendering Formats

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,10 +1,14 @@
 defmodule HelloPhoenix.PageController do
   use HelloPhoenix.Web, :controller
 
-  def index(conn, _params) do
-    conn
-    |> put_layout("admin.html")
-    |> render "index.html"
+  def index(conn, params) do
+    render conn, "index.text", message: params["message"]
+
+    # render conn, :index
+
+    # conn
+    # |> put_layout("admin.html")
+    # |> render "index.html"
 
     # |> put_flash(:info, "Welcome to Phoenix, from flash info!")
     # |> put_flash(:error, "Let's pretend we have an error")
