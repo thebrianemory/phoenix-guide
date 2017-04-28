127efaee048bbbf4603269d91b925915b327b6b1
thebrianemory <the@brianemory.com>
2017-04-27 22:04:06 -0700

    Start of testing section: adding tags

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,14 +1,18 @@
 defmodule HelloPhoenix.ErrorViewTest do
   use HelloPhoenix.ConnCase, async: true
 
+  @moduletag :error_view_case
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  @tag individual_test: "yup"
   test "renders 404.html" do
     assert render_to_string(HelloPhoenix.ErrorView, "404.html", []) ==
            "Page not found"
   end
 
+  @tag individual_test: "nope"
   test "render 500.html" do
     assert render_to_string(HelloPhoenix.ErrorView, "500.html", []) ==
            "Internal server error"
